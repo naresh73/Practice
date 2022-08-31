ef9a3f5472fd8f0e19e78756edb7f01772b04917
nareshsharma0318@gmail.com <nareshsharma0318@gmail.com>
2022-09-01 02:28:06 +0530

    added

---
+++ 3. 01-09--Redux/myreduxapp/src/balance.js
@@ -0,0 +1,10 @@
+export default function BalanceControl() {
+    return (
+        <div className="container">
+            <h2>Deposit/Withdraw Money</h2>
+            <button className="btn btn-primary mx-2">-</button>  
+            Update Balance
+            <button className="btn btn-primary mx-2">+</button>  
+        </div>
+    )
+}
