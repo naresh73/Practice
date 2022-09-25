a4dfe2831bd0c5cb3bd50d71714e6342a872c594
nareshsharma0318@gmail.com <nareshsharma0318@gmail.com>
2022-09-25 14:42:35 +0530

    redux

---
+++ React/redux/src/App.js
@@ -0,0 +1,56 @@
+import { useEffect, useState  } from 'react'
+import './App.css';
+
+function App() {
+  
+  const [val, setVal] = useState(0);
+  const [isclicked, setIsclicked] = useState(false);
+
+  useEffect(() => {
+    console.log("componentDidMount");
+}, [])
+
+
+
+useEffect(()=>{
+  console.log("componentDidUpdate")
+  return (
+    () => {
+     console.log("component will unmount")
+    }  
+   )
+},[isclicked,val])
+function handleInc() {
+  setVal(val + 1)
+}
+
+function handleDec() {
+  setVal(val - 1)
+}
+
+function handleReset() {
+  setVal(0)
+}
+
+function handleResetButton() {
+  setIsclicked(true)
+}
+
+
+  return (
+    <div className="App">
+      <h1>{val}</h1>
+      <button onClick={handleInc}>INC</button>
+      <button onClick={handleDec}>DEC</button>
+      {
+        !isclicked ? 
+      <button onClick={handleReset}>RESET</button> : 
+      null
+    }
+      <button onClick={handleResetButton}>Delete RESET</button>
+
+    </div>
+  );
+}
+
+export default App;
